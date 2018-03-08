d7ddd3dbca468a574d7e2d5f8d5466bc83da016e
筈井大策 <daisakuhazui@Vino-MAC.local>
2018-03-09 00:12:44 +0900

    fixed create-tasks-function

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,7 @@
 class Task < ApplicationRecord
   belongs_to :user, optional: true
   enum quadrant: { 必須: 0, 必要: 1, 錯覚: 2, 無駄: 3 }
+
+  validates :title, presence: true
+  validates :title, length: { maximum: 50 }
 end
