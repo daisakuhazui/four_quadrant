d977a348825bae2daf50efdba9cdee9ff6f044f8
筈井大策 <daisakuhazui@Vino-MAC.local>
2018-05-13 19:31:20 +0900

    add help_page and history_page

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,10 @@
+class StaticPagesController < ApplicationController
+  def about
+  end
+
+  def help
+  end
+
+  def history
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  get 'static_pages/about'
+  get 'static_pages/help'
+  get 'static_pages/history'
+
   root to: "tasks#index"
   devise_for :users
   resources :tasks, only: [:index, :new, :create, :destroy]
+  get "static_pages/index"
 end
