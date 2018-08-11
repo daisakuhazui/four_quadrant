4158f85f6fd14d43fde4cd20f3ca5ec2eb63d604
DaisakuHazui <daisakuhazui@Vino-MAC.local>
2018-08-11 17:09:31 +0900

    Customise tasks admin page

--- app/admin/tasks.rb
+++ app/admin/tasks.rb
@@ -1,3 +1,12 @@
 ActiveAdmin.register Task do
   permit_params :user_id, :title, :quadrant
+
+  form do |f|
+    f.inputs do
+      f.input :user
+      f.input :title
+      f.input :quadrant
+    end
+    f.actions
+  end
 end
